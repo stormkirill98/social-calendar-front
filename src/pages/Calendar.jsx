0e61ec47fa76a109ed978f506e5a7f4771280ca8
kPanihin <k.panihin@akolchin.com>
2019-12-20 18:14:58 +0300

    add months and years

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -5,7 +5,6 @@ import EventList from "../components/EventList";
 import "../css/Calendar.css"
 
 export default class Calendar extends React.Component {
-
     constructor(props) {
         super(props);
 
@@ -16,13 +15,18 @@ export default class Calendar extends React.Component {
     }
 
     render() {
+        const months = ["", "Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+
         return (
             <div className="page-container">
                 <Header/>
 
+                <div className="flex-row">
+                    <div className="year">{this.state.curYear}</div>
+                    <div className="month">{months[this.state.curMonth]}</div>
+                </div>
 
-
-                <div className="calendar-container">
+                <div className="flex-row">
                     <Month year={this.state.curYear} month={this.state.curMonth}/>
                     <EventList/>
                 </div>
